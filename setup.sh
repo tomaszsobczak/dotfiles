74ad904cd46188e28de17b986076bd66f9e4c3e6
tomaszsobczak <tomeksobczak@gmail.com>
2013-05-27 22:07:29 +0200

    Updates to setup script

--- setup.sh
+++ setup.sh
@@ -2,6 +2,25 @@
 
 # symlinks
 echo ""
+echo "==[ Preparing files ]==================="
+
+files_src=(
+    ".profile"
+)
+
+files_dst=(
+    ".env_local"
+)
+
+for k in ${!files_src[@]}; do
+    if [ -e ~/${files_dst[$k]} ]; then
+        echo "[ FAIL ] move ~/${files_src[$k]} => ~/${files_dst[$k]}"
+    else
+        mv ${files_src[$k]} ~/${files_dst[$k]}
+        echo "[  OK  ] move ~/${files_src[$k]} => ~/${files_dst[$k]}"
+    fi
+done
+
 echo "==[ Creating symlinks ]================="
 
 symlinks_src=(
@@ -18,10 +37,10 @@ symlinks_dst=(
  
 for k in ${!symlinks_src[@]}; do
     if [ -e ~/${symlinks_dst[$k]} ]; then
-        echo "[ FAIL ] ~/${symlinks_dst[$k]} => ~/.dotfiles/${symlinks_src[$k]}"
+        echo "[ FAIL ] link ~/${symlinks_dst[$k]} => ~/.dotfiles/${symlinks_src[$k]}"
     else
         ln -s ~/.dotfiles/${symlinks_src[$k]} ~/${symlinks_dst[$k]}
-        echo "[  OK  ] ~/${symlinks_dst[$k]} => ~/.dotfiles/${symlinks_src[$k]}"
+        echo "[  OK  ] link ~/${symlinks_dst[$k]} => ~/.dotfiles/${symlinks_src[$k]}"
     fi
 done
 
